9e75a7e33e0a2b6f5750f71b2d5cd50f79bbb0ef
Chibuzor Daniel <66339097+uwezukwechibuzor@users.noreply.github.com>
2022-09-20 05:00:12 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,24 +1,28 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * jack_bauer - entry point
- *
- * Return: Void
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
  */
 void jack_bauer(void)
 {
-int i, j;
+	int i, j;
 
-for (i = 0; i < 24; i++)
-{
-for (j = 0; j < 60; j++)
-{
-_putchar(i / 10 + '0');
-_putchar(i % 10 + '0');
-_putchar(':');
-_putchar(j / 10 + '0');
-_putchar(j % 10 + '0');
-_putchar('\n');
-}
-}
+	i = 0;
 
+	while (i < 24)
+	{
+		j = 0;
+		while (j < 60)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+			j++;
+		}
+		i++;
+	}
 }
