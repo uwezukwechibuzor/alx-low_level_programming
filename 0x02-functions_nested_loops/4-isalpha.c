9346c908665718652783194aa5f22e0439d7df95
Chibuzor Daniel <66339097+uwezukwechibuzor@users.noreply.github.com>
2022-09-20 04:57:49 +0100

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,17 +1,11 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * _isalpha - entry point
- * @c : bumber to check
- * Return: Always 0 (Success)
- */
+ * _isalpha - checks for alphabetic character.
+ * @c: character being checked
+ * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
+*/
 int _isalpha(int c)
 {
-if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-{
-return (1);
-}
-else
-{
-return (0);
-}
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
